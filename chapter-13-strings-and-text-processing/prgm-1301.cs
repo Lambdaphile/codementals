b9152e78ccdcb31c479e8e86521d37c6dca7f5f7
genesis algorithms <genesisalgorithms@gmail.com>
2018-11-04 18:37:43 +0200

    1 out of 25 exercises from String and Text Processing complete. :turtle:

---
+++ chapter-13-strings-and-text-processing/prgm-1301.cs
@@ -0,0 +1,32 @@
+/*
+|--------------------------------------------------------------------------
+| Chapter 13. Strings and Text Processing
+|--------------------------------------------------------------------------
+|
+| Exercise 1:
+|
+|     Write a program that reads a string, reverse it and prints it
+|     to the console. For example: "introduction" -> "noitcudortni".
+|
+| Solutions and Guidelines:
+|
+|     Use StringBuilder and for (or foreach ) loop.
+|
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
