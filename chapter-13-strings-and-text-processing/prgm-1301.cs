af5fbe4fa9c4db34e794cd91144a4f43a702a75e
genesis algorithms <genesisalgorithms@gmail.com>
2018-11-05 22:17:38 +0200

    Fixed prgm-1301.cs

--- chapter-13-strings-and-text-processing/prgm-1301.cs
+++ chapter-13-strings-and-text-processing/prgm-1301.cs
@@ -5,12 +5,12 @@
 |
 | Exercise 1:
 |
-|     Write a program that reads a string, reverse it and prints it
-|     to the console. For example: "introduction" -> "noitcudortni".
+|     Write a program that reads a string, reverse it and prints it to
+|     the console. For example: "introduction" -> "noitcudortni".
 |
 | Solutions and Guidelines:
 |
-|     Use StringBuilder and for (or foreach ) loop.
+|     Use StringBuilder and for (or foreach) loop.
 |
 */
 
@@ -22,11 +22,22 @@ using System.Threading.Tasks;
 
 namespace Program
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string ReverseString(string input)
         {
+            StringBuilder reversedInput = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+                reversedInput.Append(input[i]);
+            return reversedInput.ToString();
+        }
 
+        public static void Main(string[] args)
+        {
+            Console.Write("\nString: ");
+            string input = Console.ReadLine();
+            string reversedInput = ReverseString(input);
+            Console.WriteLine($"Reversed string: {reversedInput}");
         }
     }
 }
