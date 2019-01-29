5812dbcadbbbe6a2a1d2ce4ae195d80c267467f3
genesis algorithms <genesisalgorithms@gmail.com>
2019-01-29 23:41:32 +0200

    Improved exercise description.

--- 16-linear-data-structures/prog-1606.cs
+++ 16-linear-data-structures/prog-1606.cs
@@ -8,8 +8,7 @@
 |     Write a program that removes from a given sequence all
 |     numbers that appear an odd count of times.
 |
-|       * Example: array = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2}
-|                          -> {5, 3, 3, 5}.
+|       * array = {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2} -> {5, 3, 3, 5}.
 |
 | Solutions and Guidelines:
 |
